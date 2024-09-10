a0b81c95dda35c8d8f44ed4e8bba5e9906fbbd8d
jinsu1 <jinsu4205@gmail.com>
2024-09-11 01:47:34 +0900

    sony event, product section add

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,5 @@
 
-/* banner swiper */
+/***  banner swiper ***/
 let loop_slider = true;
 const num_of_slides = 4;
 let swiper_sliders = document.querySelectorAll(".banner_swiper");
@@ -27,12 +27,12 @@ for (const this_slider of swiper_sliders){
         },
     });
 };
+/***  banner swiper end ***/
 
 
 
 
-
-/* dual_swiper */
+/*** dual_swiper ***/
 let swiperTopNum = $('.main-slide').find('.swiper-slide');
 let swiperSubNum = $('.sub-slide').find('.sub-slide');
 
@@ -86,3 +86,51 @@ SubSlide.forEach((slide) => {
     const observer = new MutationObserver(callback);
     observer.observe(slide, { attributes: true });
 });
+
+/*** dual_swiper  end ***/
+
+
+
+
+
+/*** product get data ***/
+
+window.onload = ( async (e) => {
+    let response = null;
+
+    try {
+        response = await axios.get('assets/json/sonyProduct.json');
+        console.log(response.data);
+    } catch (error) {
+        console.error(`[Error Code] ${error.code}`);
+        console.error(`[Error message] ${error.message}`);
+        let alertMsg = error.message;
+
+        //http 상태메세지가 포함되어 있다면 해당 내용을 에러 문자열에 추가
+        if(error.response !== undefined) {
+            const errorMsg = `${error.response.status} error - ${error.response.statusText}`;
+            console.error(`[HTTP Status] ${errorMsg}`);
+            alertMsg += `\n${errorMsg}`;
+        }
+
+        alert(alertMsg);
+        return;
+    }
+
+    const prodContainer = document.querySelector(".product_container");
+
+    response.data.productList.forEach((v, i) => {
+        const prodItem = document.createElement("div");
+        const prodName = document.createElement("span");
+        
+        prodItem.classList.add("product_item");
+        prodName.innerHTML = v.prodName;
+
+        prodContainer.appendChild(prodItem);
+        prodItem.appendChild(prodName);
+        prodItem.style.backgroundImage = `url(assets/img/${v.prodImg})`;
+
+    });
+});
+
+/*** product get data end ***/
