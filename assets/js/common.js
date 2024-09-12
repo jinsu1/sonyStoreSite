8af89d4e210f1ad1fc55dfbd0e87e22c190e477f
jinsu1 <jinsu4205@gmail.com>
2024-09-13 02:07:34 +0900

    네비 이벤트 추가, gotop nav 스크롤 이벤트, pointer옵션추가 등

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,32 +1,110 @@
 /*** nav ***/
-const gnb = document.querySelector(".gnb");
+
+/* scrollEvent header && go_top append or hide */
+const headerPc = document.querySelector(".header_pc");
+const goTop = document.querySelector(".go_top");
+let lastScrollY = 0;
+
+window.addEventListener("scroll", () => {
+    const standard = 30;
+
+    if(window.scrollY > lastScrollY) {
+        //현재 스크롤 위치가 이전 위치보다 클 때 (내려갈 때)
+        headerPc.classList.add("header_pc_hide");
+        goTop.classList.add("go_top_show");
+    } else {
+        //현재 스크롤 위치가 이전 위치보다 작을 때 (올라갈 떄)
+        headerPc.classList.remove("header_pc_hide");
+        goTop.classList.remove("go_top_show");
+    }
+
+    lastScrollY = window.scrollY;
+});
+
+goTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+});
+/*
+1) class가 secondary_menu_list인 5개를 배열로 secondaryList라는 변수에 담음
+2) 1번째 depth에 마우스 올라갔을 때 2번째 depth의 ul의 배경색이 바뀌어야함
+3) 2번째 depth에 마우스 올라갔을 때 1번째 depth에 border-bottom과 color가 바뀌어야함
+4) 1번째 depth와 2번째 depth의 ul은 아빠의 사촌의 아들급인 6촌 관계임 (css hover로 상대방 제어 불가)
+5) 1번째 depth의 a태그와 2번째 depth는 ul태그 마다 for문으로 이벤트를 거는 함수를 만듦
+6) 1_depth a 와 2_depth ul은 배열의 길이가 같음
+7) 이벤트 발생지점은 i번째 요소임
+8) 각 배열의 i번째에 각각 필요한 클래스를 추가, 삭제함
+
+
+*/
 const secondary = document.querySelector(".secondary_menu_bg");
+const secondaryList = document.querySelectorAll(".secondary_menu_list");
+const gnbTitle = document.querySelectorAll(".gnb_title");
+const gnbMenuChild = document.querySelectorAll("nav > ul > li > a");
+const secondMenuChild = document.querySelectorAll("nav > ul > div > div > ul");
+
+function addEvent (el) {
+    for(let i=0;i<el.length;i++) {
+        el[i].addEventListener("mouseover", (e) => {
+            secondary.style.height = `${secondary.scrollHeight}px`;
+            gnbTitle[i].classList.add("menu_active_border");
+            secondaryList[i].classList.add("menu_active");
+            secondaryList.forEach((v, c) => {
+            });
+        });
+    }
+}
+
+function removeEvent (el) {
+    for(let i=0;i<el.length;i++) {
+        el[i].addEventListener("mouseout", (e) => {
+            secondary.style.height = `0px`;
+            secondaryList.forEach((v, c) => {
+                secondaryList[c].classList.remove("menu_active");
+                gnbTitle[i].classList.remove("menu_active_border");
+            });
+        });
+    }
+}
+addEvent(gnbMenuChild);
+addEvent(secondMenuChild);
+removeEvent(gnbMenuChild);
+removeEvent(secondMenuChild);
 
 
-gnb.addEventListener("mouseover", () => {
-    secondary.style.height = `${secondary.scrollHeight}px`;
-});
 
-gnb.addEventListener("mouseout", () => {
-    secondary.style.height = `0px`;
-});
+// 7) 1_depth a 와 2_depth ul에 dataset으로 번호를 매겨 연결점 생성
+// 8) 이벤트가 발생한 dataset.num와 2_depth ul 을 하나하나(param => c) 검사해 dataset.num이 일치하는 요소를 찾음
+// 9) 일치하는게 있다면 2_depth ul의 c번째에게 배경색을 변경하는 class추가
+// function addEvent (el) {
+//     for(let i=0;i<el.length;i++) {
+//         el[i].addEventListener("mouseover", (e) => {
+//             secondary.style.height = `${secondary.scrollHeight}px`;
+//             secondaryList.forEach((v, c) => {
+//                 if( e.currentTarget.dataset.num === v.dataset.num) {
+//                     secondaryList[c].classList.add("menu_active");
+//                     gnbTitle[c].classList.add("menu_active_border");
+//                 }
+//             });
+//         });
+//     }
+// }
 
+// function removeEvent (el) {
+//     for(let i=0;i<el.length;i++) {
+//         el[i].addEventListener("mouseout", (e) => {
+//             secondary.style.height = `0px`;
+//             secondaryList.forEach((v, c) => {
+//                 secondaryList[c].classList.remove("menu_active");
+//                 gnbTitle[i].classList.remove("menu_active_border");
+//             });
+//         });
+//     }
+// }
 
-// const menuContent = document.querySelectorAll(".secondary_menu_bg > .secondary_menu > ul > li > a");
-
-// gnb.addEventListener("mouseover", (e) => {
-//     menuContent.forEach((v, i) => {
-//         v.style.height = '54px';
-//     });
-// });
-
-
-// gnb.addEventListener("mouseout", () => {
-//     menuContent.forEach((v, i) => {
-//         v.style.height = '0';
-//     });
-//     secondary.style.height = `0px`;
-// });
 /*** nav end ***/
 
 
@@ -155,6 +233,7 @@ window.onload = ( async (e) => {
         const prodName = document.createElement("span");
         
         prodItem.classList.add("product_item");
+        prodItem.classList.add("pointer");
         prodName.innerHTML = v.prodName;
 
         prodContainer.appendChild(prodItem);
